070fbe6daa90e7bd0b078dd4ba063dda905f86e1
Celia La <celia@cockroachlabs.com>
2023-04-03 17:49:58 -0700

    test

--- pkg/clusterversion/cockroach_versions.go
+++ pkg/clusterversion/cockroach_versions.go
@@ -946,7 +946,7 @@ var rawVersionsSingleton = keyedVersions{
 // frozen. It can be forced to a specific value in two circumstances:
 // 1. forced to `false` on development branches: this is used for
 // upgrade testing purposes and should never be done in real clusters;
-// 2. forced to `false` on release branches: this allows running a
+// 2. forced to `true` on release branches: this allows running a
 // release binary in a dev cluster.
 var developmentBranch = !envutil.EnvOrDefaultBool("COCKROACH_TESTING_FORCE_RELEASE_BRANCH", false) ||
 	envutil.EnvOrDefaultBool("COCKROACH_FORCE_DEV_VERSION", false)
